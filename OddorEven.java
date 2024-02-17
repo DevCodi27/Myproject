c3df58b98ffc2411570e9e04b2e3c7942bbff7bb
Stan <standevlop@gmail.com>
2024-02-17 22:30:48 +0530

    Second commit

---
+++ OddorEven.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OddorEven {
+    public static void main(String[] args){
+        int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        number = sc.nextInt();
+
+            if(number%2==0)
+            {
+                System.out.println("Even");
+            }
+            else
+            {
+                System.out.println("Odd");
+            }
+        
+
+    }
+}
